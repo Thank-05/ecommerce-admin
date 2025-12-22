5ad3f87e95674eaa7b54247af678549f3cbfecdf
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-12-22 18:19:36 +0700

    22/12/2025 uppdate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs';
+import { ModalProvider } from "@/providers/modal-provider"; // 1. Import thêm ModalProvider
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider> {/* 2. Đã sửa: Thêm dấu > vào cuối */}
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ModalProvider /> {/* 3. Thêm dòng này để Modal hoạt động */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
